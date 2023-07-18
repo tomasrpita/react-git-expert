a44d00ae4d1f0a2de143bd5060f73d4289d315c7
Tomás R. Pita <tomasrpita@gmail.com>
2023-07-18 05:43:57 +0200

    86. className - Clases de css

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect} from 'react'
 import { getGifts } from "../helpers/getGifts";
+import { GifItem } from './GifItem';
 
 export const GifGrid = ({category})  =>  {
 
@@ -17,13 +18,16 @@ export const GifGrid = ({category})  =>  {
     return (
     <>
         <h3>{category}</h3>
-        <ol>
+        <div className='card-grid'>
           {
-            images.map(({id, title}) => (
-            <li key={id}>{title}</li>
+            images.map((image) => (
+              <GifItem 
+              key={image.id}
+              { ...image }
+              />
             ))
           }
-        </ol>
+        </div>
     </>
 
   )
